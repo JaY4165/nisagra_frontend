72af5e21dd961d4afe9da37e5c79d8d9a1534218
jay <harsha.gontu@gmail.com>
2023-02-09 13:57:24 +0530

    project setup

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginCard from "../components/LoginCard";
+
+const LoginPage = () => {
+  return (
+    <section className="">
+      <div className="flex justify-center align-center mt-[10em]">
+        <LoginCard />
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
