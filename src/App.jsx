a3d7e84872f83dde03c31c66572c3c5beaf52a71
laksh <lakshmikanth7.a@gmail.com>
2023-02-18 05:37:18 +0530

    herocommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import HomePage from "./pages/HomePage"
-import HomePage2 from "./pages/Homepage2";
-import HomePage3 from "./pages/HomePage3";
+
 
 function App() {
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import "../assets/Style1.css"
 const HomePage = () =>{
     return(
        <div className='h-screen w-screen p-4'>
-          <div className='relative'> 
-                <div className='absolute w-full h-full text-white-400  bg-black/30 flex flex-col justify center items-center  md:py-32'>
-                      <h1 className='px-4 text-4xl sm:text-5xl  md:text-6xl lg:text-7xl font-bold  '>Nisagra     Foods</h1>
-                      <h1 className='px-4 text-4xl sm:text-4xl md:text-6xl lg:text-6xl font-bold  '><span className='text-orange-400 text-4xl'>Taste the </span>Best</h1>
-                      <h1 className='px-4 text-grey-400 text-4xl sm:text-3xl  md:text-4xl lg:text-5xl font-bold md:py-4 border-green-500 border-2 border-teal-600 hover:bg-green-200 transition duration-700 ease-in-out'>Cha<span className='text-orange-700 '>lo! </span></h1>
+          <div className='sm:h-screen relative'> 
+                <div className='absolute w-full h-full text-white-400  bg-black/30 flex flex-col justify center items-center md:py-32'>
+                      <h1 className='px-4 text-6xl sm:text-5xl  md:text-6xl lg:text-9xl font-metropolis ' id="Hero">NISARGA</h1>
+                    {/* <h1 className='px-4 text-4xl sm:text-4xl  md:text-6xl lg:text-6xl font-bold  pb-20'><span className='text-orange-400 text-4xl'>Taste the </span>Best</h1>   */}
+                      <h1 className='px-4 text-grey-400 text-4xl sm:text-3xl  md:text-4xl lg:text-5xl font-bold md:py-4 border-black-500 border-2 border-teal-600 hover:bg-blue-400 transition duration-700 ease-in-out'>Cha<span className='text-orange-700 '>lo! </span></h1>
                 </div>
-                 <img className='w-full max-h-[550px] object-cover' src="https://c4.wallpaperflare.com/wallpaper/495/760/53/cuisine-food-india-indian-wallpaper-preview.jpg" alt="/" />
+                 <img className='w-full h-screen md:max-h-[550px] object-cover' src="https://c4.wallpaperflare.com/wallpaper/495/760/53/cuisine-food-india-indian-wallpaper-preview.jpg" alt="/" />
             </div>
         </div>
     )
