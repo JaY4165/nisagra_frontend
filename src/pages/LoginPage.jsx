47e5f2e8bdf2dd96f49eb448a7b81f4f18333d14
leela vathy.t <leelavathy6008@gmail.com>
2023-02-27 07:55:12 -1200

    loginpage..

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import LoginForm from "../components/logincomponents/LoginForm";
 import LoginImg from "../components/logincomponents/LoginImg";
 
@@ -11,36 +10,8 @@ const LoginPage = () => {
       
       <LoginForm/>
       <LoginImg/>
-
-      
-
       </div>
       </section>
-       
-=======
-import LoginCard from "../components/LoginCard";
-import burger1 from "../assets/burger1.jfif";
-import Login from "../components/Login";
-import New from "../components/New";
-import Loginform from "../components/Logincomponents.jsx/Loginform";
-import Loginimage from "../components/Logincomponents.jsx/Loginimage";
-
-const LoginPage = () => {
-  return (
-   <div>
-      <section className="h-[100%] w-[100%]">
-      <div className="grid md:grid-cols-2 md:grid-rows-1 sm:grid-cols-1 sm:grid-rows-2">
-      
-      
-      <Loginform/>
-      <Loginimage/>
-      </div>
-      </section>
-      
-
-      
-    </div>
->>>>>>> 90edf9fcaf4f6f05cc8b00d60d59dac5632ec267
   );
 };
 
